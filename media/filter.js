151fac3825a70754c40c1730d9098922402d7e2a
Iain Dawson <iain@bldm.us>
2013-11-29 15:18:21 +0000

    Make jshint stop yelling at me.

--- media/filter.js
+++ media/filter.js
@@ -1,31 +1,35 @@
 function parse_path(path) {
-  filter = {};
+  var filter = {};
+  var matcher = /^([^=]+)=([^=]+)$/;
+
   $.each(path.split("/"), function(i, element) {
-    matcher = /^([^=]+)=([^=]+)$/
-    match = matcher.exec(element);
+    var match = matcher.exec(element);
     if (match) {
-      filter[match[1]] = match[2]
+      filter[match[1]] = match[2];
     } else if (element) {
-      filter['title'] = element;
-    };
+      filter.title = element;
+    }
   });
   return filter;
 }
 
 function apply_filter(filter, func) {
   /* apply func(review, true) or func(review, false) depending on if a review matches */
+  var type;
+  var title;
+
   if ('title' in filter) {
-    title = filter['title']
+    title = filter.title;
     func(everything_but(title), false);
     func($('tr.review#'+title), true);
   }
   else if ('type' in filter) {
-    type = filter['type'];
+    type = filter.type;
     func(reviews_not_of_type(type), false);
     func(reviews_of_type(type), true);
   }
   else if ('stars' in filter) {
-    type = filter['stars'] + '-star';
+    type = filter.stars + '-star';
     func(reviews_not_of_type(type), false);
     func(reviews_of_type(type), true);
   }
@@ -75,12 +79,12 @@ function everything_but(title) {
   return $('tr.review:not(#'+title+')');
 }
 
-$('document').ready(function() { 
-  path = window.location.pathname
-  filter = parse_path(path);
+$('document').ready(function() {
+  var path = window.location.pathname;
+  var filter = parse_path(path);
   apply_filter(filter, visible);
 
-  filter_links = $('.type a, td.title h2 a, td.stars a, a.home')
+  var filter_links = $('.type a, td.title h2 a, td.stars a, a.home');
 
   filter_links.click(function(e) {
     $('body').removeClass('init');
@@ -100,4 +104,4 @@ $('document').ready(function() {
   }, function(e) {
     unhighlight();
   });
-})
+});
